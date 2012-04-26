ef088345585ee75c96326327ad9a695cd13f0593
Philip Tellis <philip.tellis@gmail.com>
2012-04-26 16:18:16 -0400

    copy sysctl and limits configs to appropriate places

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,1 +1,5 @@
 apt-get install make pkg-config git git-man gcc g++ libssl-dev uuid-dev htop curl
+cwd=`pwd`
+ln -s $cwd/etc/sysctl.conf /etc/sysctl.d/60-network-limits.conf
+cp $cwd/etc/security/limits.d/nofile.conf /etc/security/limits.d/nofile.conf
+
