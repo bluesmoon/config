9a495cc5603b207caf5d3e9d4ca91409e622855a
Philip Tellis <philip.tellis@gmail.com>
2012-04-28 02:32:49 -0400

    upgrade apt packages and set vi as editor by default

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,5 +1,9 @@
-apt-get install make pkg-config git git-man gcc g++ libssl-dev uuid-dev htop curl
+apt-get update
+apt-get -y upgrade
+apt-get -y install make pkg-config git git-man gcc g++ libssl-dev uuid-dev htop curl
+
 cwd=`pwd`
 ln -s $cwd/etc/sysctl.conf /etc/sysctl.d/60-network-limits.conf
 cp $cwd/etc/security/limits.d/nofile.conf /etc/security/limits.d/nofile.conf
 
+echo "EDITOR=vi" | sudo tee -a /etc/bash.bashrc >/dev/null
