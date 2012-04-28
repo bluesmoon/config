35b383bb37f8e59352e3ba22d06ecb5b39f600c2
Philip Tellis <philip.tellis@gmail.com>
2012-04-28 02:34:19 -0400

    set default locale to C

--- linux/setup.sh
+++ linux/setup.sh
@@ -5,5 +5,6 @@ apt-get -y install make pkg-config git git-man gcc g++ libssl-dev uuid-dev htop 
 cwd=`pwd`
 ln -s $cwd/etc/sysctl.conf /etc/sysctl.d/60-network-limits.conf
 cp $cwd/etc/security/limits.d/nofile.conf /etc/security/limits.d/nofile.conf
+cp $cwd/etc/default/locale /etc/default/locale
 
 echo "EDITOR=vi" | sudo tee -a /etc/bash.bashrc >/dev/null
