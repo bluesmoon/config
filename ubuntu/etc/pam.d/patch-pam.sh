2f444b5c65f90b56f754884c056839661590225c
Philip Tellis <philip.tellis@gmail.com>
2012-10-26 11:24:31 -0400

    invert condition because ! does not appear to work correctly

--- ubuntu/etc/pam.d/patch-pam.sh
+++ ubuntu/etc/pam.d/patch-pam.sh
@@ -4,7 +4,9 @@ FILES="common-session common-session-noninteractive"
 
 for f in $FILES; do
 	f="/etc/pam.d/$f"
-	if !grep "^session[ 	][ 	]*required[ 	][ 	]*pam_limits\.so\>" $f &>/dev/null; then
+	if grep "^session[ 	][ 	]*required[ 	][ 	]*pam_limits\.so\>" $f &>/dev/null; then
+		echo "$f OK"
+	else
 		echo "" >> $f
 		echo "# Added by patch-pam.sh from https://github.com/lognormal/config" >> $f
 		echo "session	required	pam_limits.so" >> $f
