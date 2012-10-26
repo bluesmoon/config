a5dd9f2bc31d1a214d3a86cc596bb1ac89bc354b
Philip Tellis <philip.tellis@gmail.com>
2012-10-26 10:48:33 -0400

    Add pam_limits.so to common-session*
    We need pam_limits to run through limits.conf.  On Ubuntu this isn't
    included in common-session by default, so this code adds it.

--- ubuntu/etc/pam.d/patch-pam.sh
+++ ubuntu/etc/pam.d/patch-pam.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+FILES="common-session common-session-noninteractive"
+
+for f in $FILES; do
+	if !grep "^session[ 	][ 	]*required[ 	][ 	]*pam_limits\.so\>" $f &>/dev/null; then
+		echo "" >> $f
+		echo "# Added by patch-pam.sh from https://github.com/lognormal/config" >> $f
+		echo "session	required	pam_limits.so" >> $f
+		echo "# End" >> $f
+	fi
+done
